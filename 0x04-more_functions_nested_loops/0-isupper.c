428ceab1fded641f31fa6110aa4f42320ecd8c33
dgyamerah <gyamerahdeladem@gmail.com>
2022-12-16 13:59:55 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a character is uppercase
+ * @x: the number to be checked
+ * 
+ * Return: 1 if character is uppercase and 0 if otherwise
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
