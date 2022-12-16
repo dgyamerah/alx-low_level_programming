dd1140bbfb97465e541733d09ac5de62507fd7b9
dgyamerah <gyamerahdeladem@gmail.com>
2022-12-16 04:39:47 -0800

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,11 @@ int main(void)
 	printf("Infinte loop incoming :(\n");
 
 	i = 0;
-
 	/*while (i < 10)*/
 	/*{*/
 		/*putchar(i);*/
 	/*}*/
 
 	printf("Infinite loop avoided! \\o\/\n");
-
 	return (0);
 }
